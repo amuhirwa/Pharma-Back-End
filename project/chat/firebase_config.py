5ed1a090c2ad8abed0807be9e94ee9160faa125e
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2025-07-25 22:35:14 +0200

    Update firebase_config.py

--- project/chat/firebase_config.py
+++ project/chat/firebase_config.py
@@ -5,7 +5,7 @@ import firebase_admin
 from decouple import config
 from firebase_admin import credentials
 
-firebase_adminsdk_json = base64.b64decode(config('FIREBASE_ADMINSDK_JSON'))
+firebase_adminsdk_json = base64.b64decode(config('FIREBASE_ADMINSDK_JSON', ''))
 firebase_adminsdk_dict = json.loads(firebase_adminsdk_json)
 
 
