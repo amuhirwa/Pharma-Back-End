cdff6b452a4d4c162db34806acd71bd4df971b18
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2025-07-25 23:41:39 +0200

    Update production.py

--- project/project/settings/production.py
+++ project/project/settings/production.py
@@ -4,15 +4,14 @@ from decouple import config
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DATABASE_NAME", "pharcy"),
-        "USER": config("DATABASE_USER", "my_testdatabase_user"),
-        "PASSWORD": config("DATABASE_PASSWORD", "ogQSffCkoF4kDoBIzVKI4MCTJtmj1l07"),
-        "HOST": config("DATABASE_HOST", "dpg-d21ve8e3jp1c738c2rv0-a"),
-        "PORT": config("PORT",default="5432"),
+        "NAME": "my_testdatabase",
+        "USER": "my_testdatabase_user",
+        "PASSWORD": "ogQSffCkoF4kDoBIzVKI4MCTJtmj1l07",
+        "HOST": "dpg-d21ve8e3jp1c738c2rv0-a.oregon-postgres.render.com",
+        "PORT": "5432",                    # default Postgres port
         "TEST": {
             "NAME": "my_testdatabase",
         },
-       "ATOMIC_REQUESTS": True
-
+        "ATOMIC_REQUESTS": True,
     }
 }
